61d8a0e52624092cb3a41df5eeb885d5081bf89c
waringme <waring@adobe.com>
2026-08-28 13:54:01 +0100

    Implement conditional loading of dapreview.js
    Add async function to load 'dapreview.js' if parameter is present in URL.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -468,3 +468,8 @@ async function loadPage() {
 }
 
 loadPage();
+
+(async function loadDa() {
+  if (!new URL(window.location.href).searchParams.get('dapreview')) return;
+  import('https://da.live/scripts/dapreview.js').then(({ default: daPreview }) => daPreview(loadPage));
+}());
